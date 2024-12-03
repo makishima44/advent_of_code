ab1658434fe795ab9aadb0bb5318637b9e28512c
Artur <metallist15431543@gmail.com>
2024-12-03 20:35:15 +0300

    day1 part2

--- day1/main.js
+++ day1/main.js
@@ -1,8 +1,7 @@
-// Part 1
-
 const fs = require("fs");
 
-function solve(file) {
+//------------------------------ Part 1---------------------------------------------//
+function solve1(file) {
   const data = fs.readFileSync(file, "utf-8");
   const rows = data.split("\n").map((row) => row.replace("\r", ""));
 
@@ -19,6 +18,7 @@ function solve(file) {
   let sortColumn2 = column2.sort((a, b) => a - b);
 
   let different = 0;
+
   const minLength = Math.min(sortColumn1.length, sortColumn2.length);
 
   for (let i = 0; i < minLength; i++) {
@@ -28,8 +28,36 @@ function solve(file) {
   return different;
 }
 
-const result = solve("./day1/text.txt");
+const result = solve1("./day1/text.txt");
 
-console.log(result);
+// console.log(result);
 
-// Part 2
+//------------------------------ Part 2 ---------------------------------------------//
+function solve2(file) {
+  const data = fs.readFileSync(file, "utf-8");
+  const rows = data.split("\n").map((row) => row.replace("\r", ""));
+
+  let column1 = [];
+  let column2 = [];
+
+  rows.forEach((row) => {
+    const [num1, num2] = row.split(/\s+/);
+    column1.push(Number(num1));
+    column2.push(Number(num2));
+  });
+
+  const minLength = Math.min(column1.length, column2.length);
+
+  let score = 0;
+
+  for (let i = 0; i < minLength; i++) {
+    let count = column2.filter((el) => el === column1[i]).length;
+    score += count * column1[i];
+  }
+
+  return score;
+}
+
+const result2 = solve2("./day1/text.txt");
+
+// console.log(result2);
